257122d2e612dee0fd937364e3f5690385b36d8c
Criss Villegas <mcxcrissx@gmail.com>
2018-02-18 22:20:26 -0600

    Add errores verdadero y aproximado

--- Tarea 1/error.m
+++ Tarea 1/error.m
@@ -1,17 +1,41 @@
-format long;
-function result = expoError(n, x)
+#calcula el estimado y su error
+
+function currEstimate = expoError(n, x)
+    realValue = exp(x);
+    preEstimate = 0;
     kFac = 1;
     xToTheK = 1;
-    result = 0;
-    for i = (1:n-1)
-        result += xToTheK / kFac;
-        printf("%.5g / %.5g = %.5g acumulate %.5g\n", xToTheK, kFac, xToTheK/kFac, result);
-
+    currEstimate = 0;
+    eaE = 100;
+    i = 1; #counter
+    esE = scarboroughError(n);
+    etE = 100;
+    printf("The real value is: %.10g\n", realValue);
+    printf("N\tCurrent Value       \t\tReal Error\t      Aproximate Error\n");
+    while (eaE > esE)
+        etE = (realValue - currEstimate) * 100 / realValue;
+        preEstimate = currEstimate;
+        currEstimate += xToTheK / kFac;
+        eaE = (currEstimate - preEstimate) * 100 / currEstimate;
         xToTheK *= x;
         kFac *= i;
-    endfor
+        printf("%2d\t", i);
+        printf("%20.13g\t", currEstimate);
+        printf("%20.13g    \t",etE);
+        printf("%20.13g\n", eaE);
+        ++i;
+    endwhile
     return;
 endfunction
 
-r = expoError(1000, 2);
-printf("result: %.5g\n", r);
+#calcula el error de scarborough
+function es = scarboroughError(n)
+    dec = 1;
+    for i = (1:n)
+        dec /= 10;
+    endfor
+    es=(0.5 * dec)*100;
+    printf("The threshold is: %.20g\n", es);
+    return;
+endfunction
+
